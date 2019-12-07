94c61611fc164c9f39263a3172db9b89671f4bb4
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-07 10:18:09 +0330

    added cors

--- tedx/settings/dev.py
+++ tedx/settings/dev.py
@@ -10,3 +10,35 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+INSTALLED_APPS += [
+    'corsheaders',
+]
+MIDDLEWARE = [
+                 'corsheaders.middleware.CorsMiddleware',
+             ] + MIDDLEWARE
+
+# CORS Settings Section
+# https://github.com/adamchainz/django-cors-headers
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+
+    'Access-Control-Allow-Origin',
+]
